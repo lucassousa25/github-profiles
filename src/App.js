01d5d21e9ccff26c93f25881644f0b024d401eb8
lucassousa25 <lucassousao2510@gmail.com>
2022-02-05 20:23:44 -0300

    Component Layout created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Layout from './components/layout';
 
 function App() {
   return (
-    <div className="App">GitHub Profiles</div>
+    <main>
+      <Layout />
+    </main>
   );
 }
 
