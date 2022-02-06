c2381a746b545863c9900b480b7860ab1cb5c544
lucassousa25 <lucassousao2510@gmail.com>
2022-02-05 21:19:54 -0300

    Profile component created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import Layout from './components/layout';
 
-function App() {
+const App = () => {
   return (
     <main>
-      <Layout />
+      <Layout>
+        <div>
+          <img src="https://avatars.githubusercontent.com/u/32560743?v=4" alt="Avatar User's Profile" />
+          <h1>Lucas S. Oliveira</h1>
+          <h3>Username: </h3><span>Lucas S</span>
+          <div>
+            <div>
+              <h4>Followers:</h4>
+              <span>59</span>
+            </div>
+            <div>
+              <h4>Starreds:</h4>
+              <span>12</span>
+            </div>
+            <div>
+              <h4>Followings:</h4>
+              <span>102</span>
+            </div>
+          </div>
+        </div>
+        <div>Repositories</div>
+        <div>Starred</div>
+      </Layout>
     </main>
   );
 }
---
+++ src/components/profile/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as S from './styled'
+
+const Profile = () => {
+  return (
+    <S.Wrapper>
+      
+    </S.Wrapper>
+  );
+}
+
+export default Profile;
