03fac80226acd98160eafc0dae612f628df70fbc
lucassousa25 <lucassousao2510@gmail.com>
2022-02-05 23:12:36 -0300

    Adjusting user profile references

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -4,7 +4,36 @@ import * as S from './styled'
 const Profile = () => {
   return (
     <S.Wrapper>
-      
+      <S.WrapperImage src="https://avatars.githubusercontent.com/u/32560743?v=4" alt="Avatar User's Profile" />
+      <S.WrapperInfoUser>
+        <div>
+          <h1>Lucas S. Oliveira</h1>
+          <S.WrapperUsername>
+            <h3>Username:</h3>
+            <a 
+              href='https://github.com/lucassousa25'
+              target='_blank'
+              rel='noreferrer'
+            >
+              Lucas S
+            </a>
+          </S.WrapperUsername>
+        </div>
+        <S.WrapperStatusCount>
+          <div>
+            <h4>Followers</h4>
+            <span>59</span>
+          </div>
+          <div>
+            <h4>Starreds</h4>
+            <span>12</span>
+          </div>
+          <div>
+            <h4>Followings</h4>
+            <span>102</span>
+          </div>
+        </S.WrapperStatusCount>
+      </S.WrapperInfoUser>
     </S.Wrapper>
   );
 }
--- src/components/profile/styled.js
+++ src/components/profile/styled.js
@@ -1,3 +1,38 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: flex-start;
+`;
+
+export const WrapperInfoUser = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: space-between;
+  height: auto;
+  margin-left: 10px;
+`;
+
+export const WrapperStatusCount = styled.div`
+  display: flex;
+  align-items: center;
+  div {
+    margin: 10px;
+    text-align: center;
+  }
+`;
+
+export const WrapperUsername = styled.div`
+  display: flex;
+  align-items: center;
+  span {
+    margin-left: 10px;
+  }
+`;
+
+export const WrapperImage = styled.img`
+  border-radius: 50%;
+  width: 15%;
+  margin: 10px;
+`;
