74df2d12cb0f06ca5ecb4d39fee1af15bf72accf
lucassousa25 <lucassousao2510@gmail.com>
2022-04-09 21:40:31 -0300

    Adjustyng Semantic body tag

--- src/components/layout/styled.js
+++ src/components/layout/styled.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const WrapperLayout = styled.body`
+export const WrapperLayout = styled.div`
   position: absolute;
   width: 100%;
   height: 100vh;
